ddd907531e2b92d98a51ee6e9edc9c66cb16ee74
AustinWittenburg <79556945+AustinWittenburg@users.noreply.github.com>
2022-02-02 20:29:45 -0600

    Create Transmitter.ino
    Added the early version of our transmitter code that works with an arduino

---
+++ Transmitter.ino
@@ -0,0 +1,48 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 2 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+*
+* THIS IS FOR TRANSMITTING WITH AN ARDUINO NOT ATTINY
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define led 12
+
+RF24 radio(3, 3); // CE, CSN
+const byte addresses[][6] = {"00001", "00002"};
+boolean buttonState = 0;
+
+void setup() {
+  pinMode(12, OUTPUT);
+  radio.begin();
+  radio.openWritingPipe(addresses[1]); // 00002
+  radio.openReadingPipe(1, addresses[0]); // 00001
+  radio.setPALevel(RF24_PA_MIN);
+}
+
+void loop() {
+  delay(5);
+
+  radio.stopListening();
+  int potValue = analogRead(A0);
+  int angleValue = map(potValue, 0, 1023, 0, 180);
+  radio.write(&angleValue, sizeof(angleValue));
+
+  delay(5);
+  radio.startListening();
+  while (!radio.available());
+  radio.read(&buttonState, sizeof(buttonState));
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
+}
