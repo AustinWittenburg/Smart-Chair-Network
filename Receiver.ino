65629378f993047f67f34469eb27db061c793ee8
AustinWittenburg <79556945+AustinWittenburg@users.noreply.github.com>
2022-02-14 19:40:37 -0600

    Update Receiver.ino
    Updated the receiver code from the NRFLite library

--- Receiver.ino
+++ Receiver.ino
@@ -1,35 +1,73 @@
 /*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* THIS WORKS WITH ARDUINO
-*
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+
+Demonstrates simple RX and TX operation.
+Any of the Basic_TX examples can be used as a transmitter.
+Please read through 'NRFLite.h' for a description of all the methods available in the library.
+
+Works using the Arduino as the hub for the received message.
+
+Radio    Arduino
+CE    -> 9
+CSN   -> 10 (Hardware SPI SS)
+MOSI  -> 11 (Hardware SPI MOSI)
+MISO  -> 12 (Hardware SPI MISO)
+SCK   -> 13 (Hardware SPI SCK)
+IRQ   -> No connection
+VCC   -> No more than 3.6 volts
+GND   -> GND
+
 */
 
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include "SPI.h"
+#include "NRFLite.h"
 
-RF24 radio(7, 8); // CE, CSN
+const static uint8_t RADIO_ID = 0;       // Our radio's id.  The transmitter will send to this id.
+const static uint8_t PIN_RADIO_CE = 9;
+const static uint8_t PIN_RADIO_CSN = 10;
 
-const byte address[6] = "00001";
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+    uint8_t FromRadioId;
+    uint32_t OnTimeMillis;
+    uint32_t FailedTxCount;
+};
 
-void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+NRFLite _radio;
+RadioPacket _radioData;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
+    // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+    // You can run the 'ChannelScanner' example to help select the best channel for your environment.
+    // You can assign a different bitrate and channel as shown below.
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+    
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+    {
+        Serial.println("Cannot communicate with radio");
+        while (1); // Wait here forever.
+    }
 }
 
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.print("Message being received: ");
-    Serial.println(text);
-  }
+void loop()
+{
+    while (_radio.hasData())
+    {
+        _radio.readData(&_radioData); // Note how '&' must be placed in front of the variable name.
+
+        String msg = "Radio ";
+        msg += _radioData.FromRadioId;
+        msg += ", ";
+        msg += _radioData.OnTimeMillis;
+        msg += " ms, ";
+        msg += _radioData.FailedTxCount;
+        msg += " Failed TX";
+
+        Serial.println(msg);
+    }
 }
